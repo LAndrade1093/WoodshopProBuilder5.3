e3d996c433d9160a22ab02cf636b6f7be86b2b1a
Luis Andrade <LAndrade1093@gmail.com>
2016-06-26 18:01:42 -0600

    UI Changes and Projects UI
    Changed UI for main menu and added UI for displaying the Projects
    available

---
+++ Assets/Scripts/UI/ProjectButtonUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ProjectButtonUI : MonoBehaviour
+{
+    public Text ProjectName;
+    public Text PriceText;
+    public Text ScoreText;
+    public Image ProjectImage;
+}
---
+++ Assets/Scripts/UI/ProjectPanelController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProjectPanelController : MonoBehaviour
+{
+    public GameObject ProjectButtonPrefab;
+
+    private List<ProjectButtonUI> projectButtons;
+
+	void Start ()
+    {
+        foreach(Project p in ProjectsDatabase.RetrieveAllProjects())
+        {
+
+        }
+	}
+}
