3f0b462cb406e37d8ba50e94665a1025b21fc49b
Luis Andrade <LAndrade1093@gmail.com>
2016-08-22 11:20:11 -0600

    Updated test file

--- Assets/CSVImportTest.cs
+++ Assets/CSVImportTest.cs
@@ -12,64 +12,26 @@ public class CSVImportTest : MonoBehaviour {
     public void ImportFromCSV()
     {
         TextAsset t = Resources.Load("GameCSVData/testingImport") as TextAsset;
-        if(t == null)
+        if (t == null)
         {
-            Debug.Log("Not found");
+            TextDisplay.text = "\"GameCSVData/testingImport\" was not found";
         }
         else
         {
-            Debug.Log("Found");
-        }
-        string text = t.text;
-        string loadedData = "";
-        string[] listOfText = text.Split('\n');
-        foreach(string line in listOfText)
-        {
-            if (line.Length > 0)
+            StreamReader sr = new StreamReader(new MemoryStream(t.bytes));
+            string loadedData = "";
+            while (!sr.EndOfStream)
             {
-                string dataLine = "";
-                string[] dataList = line.Split(',');
-                foreach (string data in dataList)
+                string[] line = sr.ReadLine().Split(',');
+                string loadedText = "";
+                foreach (string data in line)
                 {
-                    dataLine += data + " | ";
+                    loadedText += data + " | ";
                 }
-                loadedData += dataLine + "\n";
-                Debug.Log(dataLine);
+                loadedData += loadedText + "\n";
+                Debug.Log(loadedText + "\n");
             }
+            TextDisplay.text = loadedData;
         }
-        TextDisplay.text = loadedData;
     }
 }
-
-//string text = @"" + Application.dataPath + "/Resources/GameCSVData/testingImport.csv";
-//StreamReader sr = new StreamReader(text);
-//int lines = 1;
-//string AllText = "";
-//while (!sr.EndOfStream)
-//{
-//    string[] line = sr.ReadLine().Split(',');
-//    string loadedText = "";
-//    foreach (string data in line)
-//    {
-//        loadedText += data + " | ";
-//    }
-//    AllText += "Line " + lines + ": " + loadedText+"\n";
-//    Debug.Log("Line "+lines+":" + loadedText);
-//    lines++;
-//}
-//TextDisplay.text = AllText;
-
-
-
-
-
-
-
-//text += "Data Path: "+Application.dataPath+"\n\n";
-//text += "Persistant Data Path: " + Application.persistentDataPath+"\n\n";
-
-//TextAsset csv = (TextAsset)Resources.Load("GameCSVData/testingImport", typeof(TextAsset));
-//text += "Does \"testingImport.csv\" exists: ";
-//text += (csv != null) ? "Yes" : "No";
-//Debug.Log(text);
-//TextDisplay.text = text;
